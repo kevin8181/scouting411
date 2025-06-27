0d5a73df873253075914bfd2a8c2a69e3b517f09
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-06-27 15:48:11 -0400

    ui refinement

--- src/features/resources/components/Link.tsx
+++ src/features/resources/components/Link.tsx
@@ -3,7 +3,7 @@ import type { Resource } from "@/features/resources/types";
 export function Link({ resource }: { resource: Resource }) {
 	return (
 		<div className="shadow-card flex flex-col justify-between gap-3 rounded bg-white p-6 md:flex-row">
-			<div>
+			<div className="flex flex-col gap-3">
 				<a
 					href={resource.url + "?utm_source=scouting411"}
 					rel="noopener noreferrer"
