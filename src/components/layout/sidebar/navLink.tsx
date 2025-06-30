c77be9c0357aff7545f4dc2fb75734e8161f61cd
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-06-29 23:34:04 -0400

    add icons

--- src/components/layout/sidebar/navLink.tsx
+++ src/components/layout/sidebar/navLink.tsx
@@ -1,13 +1,18 @@
+import type { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function NavLink({
 	href,
 	label,
 	newTab,
 	currentUrl,
+	icon,
 }: {
 	href: string;
 	label: string;
 	newTab?: boolean;
 	currentUrl: URL;
+	icon: FontAwesomeIconProps["icon"];
 }) {
 	const isActive = currentUrl.pathname === href;
 
@@ -19,13 +24,13 @@ export function NavLink({
 		<a
 			href={href}
 			className={
-				"hover:border-brand-gray-3 rounded-lg border px-3 py-2 text-sm " +
+				"hover:border-brand-gray-3 rounded-lg border px-3 py-2 text-sm flex items-center gap-2.5 " +
 				activeStateCss
 			}
 			target={newTab ? "_blank" : undefined}
 			rel={newTab ? "noopener noreferrer" : undefined}
 		>
-			{label}
+			<FontAwesomeIcon icon={icon} height={"1em"}></FontAwesomeIcon> {label}
 		</a>
 	);
 }
--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -1,12 +1,29 @@
 import { NavLink } from "@/components/layout/sidebar/navLink";
+import {
+	faBookBookmark,
+	faHome,
+	faNewspaper,
+	faSquareRss,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGitAlt } from "@fortawesome/free-brands-svg-icons";
 
 export default function Sidebar({ url }: { url: URL }) {
 	return (
 		<aside className="bg-brand-gray-1 border-brand-gray-3 hidden w-64 min-w-64 flex-col justify-between border-r xl:flex">
 			<nav className="text-brand-gray-10 flex flex-col gap-[2px] p-3">
-				<NavLink href="/" label="Home" currentUrl={url} />
-				<NavLink href="/feed" label="Feed" currentUrl={url} />
-				<NavLink href="/resources" label="Resources" currentUrl={url} />
+				<NavLink href="/" label="Home" currentUrl={url} icon={faHome} />
+				<NavLink
+					href="/feed"
+					label="Feed"
+					currentUrl={url}
+					icon={faNewspaper}
+				/>
+				<NavLink
+					href="/resources"
+					label="Resources"
+					currentUrl={url}
+					icon={faBookBookmark}
+				/>
 			</nav>
 			<div>
 				<nav className="flex flex-col p-3">
@@ -15,12 +32,14 @@ export default function Sidebar({ url }: { url: URL }) {
 						label="RSS Feed"
 						newTab
 						currentUrl={url}
+						icon={faSquareRss}
 					/>
 					<NavLink
 						href="https://github.com/kevin8181/scouting411"
 						label="GitHub"
 						newTab
 						currentUrl={url}
+						icon={faGitAlt}
 					/>
 				</nav>
 				<div className="p-3 text-xs">
