e31837144391acde11d4ab4cf0b896ba7d9a043a
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-07-07 02:19:59 -0400

    symbol decoding using he

--- src/components/react/post.tsx
+++ src/components/react/post.tsx
@@ -1,5 +1,8 @@
 import type { Post, PostWithFeed } from "@/features/feeds/types";
 
+//todo move this symbol decoding somewhere else; it shouldn't be in the display layer
+import he from "he";
+
 export function Post({ post }: { post: PostWithFeed }) {
 	return (
 		<div className="border-brand-gray-3 flex flex-col items-start gap-3 rounded-lg border bg-white p-6">
@@ -9,10 +12,10 @@ export function Post({ post }: { post: PostWithFeed }) {
 				target="_blank"
 				className="text-brand-blue font-serif text-xl font-bold wrap-anywhere hyphens-auto hover:underline"
 			>
-				{post.title}
+				{he.decode(post.title)}
 			</a>
 			<span className="text-sm wrap-anywhere hyphens-auto">
-				{post.description}
+				{he.decode(post.description ?? "No excerpt available.")}
 			</span>
 			<span className="flex items-center gap-2 text-xs text-gray-700">
 				{/* todo make this a link/button that does something */}
