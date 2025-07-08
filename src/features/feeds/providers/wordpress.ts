e8ace9fb525799fdfec1edf4b4b99f8582e6a87b
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-07-07 23:55:52 -0400

    get wp adapter ready for pagination

--- src/features/feeds/providers/wordpress.ts
+++ src/features/feeds/providers/wordpress.ts
@@ -1,36 +1,72 @@
 import type { FeedProvider } from "@/features/feeds/types";
+import type { PostData } from "@/features/feeds/types";
 import he from "he";
 
-//todo fetch the full post history
 
 export function wordpressProvider(opts: wordpressProviderOpts): FeedProvider {
+	async function fetchPage(page: number) {
+		const url = new URL(
+			`/wp-json/wp/v2/posts?page=${page}&per_page=100${opts.categoryFilter ? `&categories=${opts.categoryFilter}` : ""}`,
+			opts.baseUrl,
+		).toString();
+
+		const response = await fetch(url);
+
+		if (response.status !== 200) {
+			throw new Error(
+				`failed to fetch posts from ${url}- status code ${response.status}`,
+			);
+		}
+
+		const rawPosts: wordpressApiPost[] = await response.json();
+
+		const posts = rawPosts.map((post) => ({
+			url: post.link,
+			title: he.decode(post.title.rendered),
+			description: he.decode(
+				post.yoast_head_json?.og_description ?? post.excerpt.rendered,
+			),
+			date: new Date(post.date_gmt),
+		}));
+
+		return {
+			posts,
+			totalPages: parseInt(response.headers.get("x-wp-totalpages")!),
+		};
+	}
+
 	return {
 		type: "wordpress",
 
 		fetch: async () => {
-			const url = new URL(
-				`/wp-json/wp/v2/posts?per_page=100${opts.categoryFilter ? `&categories=${opts.categoryFilter}` : ""}`,
-				opts.baseUrl,
-			).toString();
+			const posts: PostData[] = [];
 
-			const response = await fetch(url);
+			const firstPage = await fetchPage(1);
+			posts.push(...firstPage.posts);
 
-			if (response.status !== 200) {
-				throw new Error(
-					`failed to fetch posts from ${url}- status code ${response.status}`,
-				);
+			if (firstPage.totalPages === 1) {
+				return posts;
 			}
 
-			const posts: wordpressApiPost[] = await response.json();
+			//todo fetch the full post history
 
-			return posts.map((post) => ({
-				url: post.link,
-				title: he.decode(post.title.rendered),
-				description: he.decode(
-					post.yoast_head_json?.og_description ?? post.excerpt.rendered,
-				),
-				date: new Date(post.date_gmt),
-			}));
+			// console.log(`fetching ${firstPage.totalPages} pages from ${opts.baseUrl}`)
+
+			// // create an array where each entry is a page number we need to fetch
+			// const arr = Array.from(
+			// 	{ length: firstPage.totalPages - 1 },
+			// 	(_, i) => i + 2,
+			// );
+
+			// Promise.all(arr.map(async (pageNumber) => {
+			// 	console.log(`fetching page ${pageNumber} of ${opts.baseUrl}`)
+			// 	const page = await fetchPage(pageNumber);
+			// 	posts.push(...page.posts);
+			// }))
+
+			// console.log(`finished fetching ${opts.baseUrl}`)
+
+			return posts;
 		},
 	};
 }
