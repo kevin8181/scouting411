ce5d3b12e491e4790d11959dba4d46fc540a8011
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-07-09 13:17:18 -0400

    throttle

--- src/features/feeds/providers/wordpress.ts
+++ src/features/feeds/providers/wordpress.ts
@@ -1,68 +1,62 @@
 import type { FeedProvider } from "@/features/feeds/types";
 import type { PostData } from "@/features/feeds/types";
 import he from "he";
+import { promiseAllDelayed } from "@/util/promiseAllDelayed";
 
-export function wordpressProvider(opts: wordpressProviderOpts): FeedProvider {
-	async function fetchPage(page: number) {
-		const url = new URL(
-			`/wp-json/wp/v2/posts?page=${page}&per_page=100${opts.categoryFilter ? `&categories=${opts.categoryFilter}` : ""}`,
-			opts.baseUrl,
-		).toString();
+async function fetchPage(page: number, opts: wordpressProviderOpts) {
+	console.log(`fetch page ${page} from ${opts.baseUrl}`);
 
-		const response = await fetch(url);
+	const url = new URL(
+		`/wp-json/wp/v2/posts?page=${page}&per_page=100${opts.categoryFilter ? `&categories=${opts.categoryFilter}` : ""}`,
+		opts.baseUrl,
+	).toString();
 
-		if (response.status !== 200) {
-			throw new Error(
-				`failed to fetch posts from ${url}- status code ${response.status}`,
-			);
-		}
+	const response = await fetch(url);
 
-		const rawPosts: wordpressApiPost[] = await response.json();
-
-		const posts = rawPosts.map((post) => ({
-			url: post.link,
-			title: he.decode(post.title.rendered),
-			description: he.decode(
-				post.yoast_head_json?.og_description ?? post.excerpt.rendered,
-			),
-			date: new Date(post.date_gmt),
-		}));
-
-		return {
-			posts,
-			totalPages: parseInt(response.headers.get("x-wp-totalpages")!),
-		};
+	if (response.status !== 200) {
+		throw new Error(
+			`failed to fetch posts from ${url}- status code ${response.status}`,
+		);
 	}
 
+	const rawPosts: wordpressApiPost[] = await response.json();
+
+	const posts = rawPosts.map((post) => ({
+		url: post.link,
+		title: he.decode(post.title.rendered),
+		description: he.decode(
+			post.yoast_head_json?.og_description ?? post.excerpt.rendered,
+		),
+		date: new Date(post.date_gmt),
+	}));
+
+	return {
+		posts,
+		totalPages: parseInt(response.headers.get("x-wp-totalpages")!),
+	};
+}
+
+export function wordpressProvider(opts: wordpressProviderOpts): FeedProvider {
 	return {
 		type: "wordpress",
 
 		fetch: async () => {
 			const posts: PostData[] = [];
 
-			const firstPage = await fetchPage(1);
-			posts.push(...firstPage.posts);
+			const totalPages = (await fetchPage(1, opts)).totalPages;
 
-			if (firstPage.totalPages === 1) {
-				return posts;
-			}
+			// create an array where each element is a function that fetches a page
+			const functions = Array.from({ length: totalPages }, (_, i) => {
+				return async () => await fetchPage(i + 1, opts);
+			});
 
-			//todo fetch the full post history
+			const pages = await promiseAllDelayed(functions, 100);
 
-			// create an array where each entry is a page number we need to fetch
-			const arr = Array.from(
-				{ length: firstPage.totalPages - 1 },
-				(_, i) => i + 2,
-			);
+			pages.forEach((page) => {
+				posts.push(...page.posts);
+			});
 
-			await Promise.all(
-				arr.map(async (pageNumber) => {
-					const page = await fetchPage(pageNumber);
-					posts.push(...page.posts);
-				}),
-			);
-
-			console.log(`fetched ${posts.length} posts from ${opts.baseUrl}`);
+			console.log(`got ${posts.length} posts from ${opts.baseUrl}`);
 
 			return posts;
 		},
