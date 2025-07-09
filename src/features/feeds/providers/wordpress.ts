d2b48c3b028a75724442181aece32043df846465
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-07-09 11:57:28 -0400

    wordpress provider throttling

--- src/features/feeds/providers/wordpress.ts
+++ src/features/feeds/providers/wordpress.ts
@@ -1,38 +1,46 @@
 import type { FeedProvider } from "@/features/feeds/types";
 import type { PostData } from "@/features/feeds/types";
 import he from "he";
+import { asyncThrottle } from "@tanstack/pacer";
+
+async function fetchPage(page: number, opts: wordpressProviderOpts) {
+	const url = new URL(
+		`/wp-json/wp/v2/posts?page=${page}&per_page=100${opts.categoryFilter ? `&categories=${opts.categoryFilter}` : ""}`,
+		opts.baseUrl,
+	).toString();
+
+	const response = await fetch(url);
+
+	if (response.status !== 200) {
+		throw new Error(
+			`failed to fetch posts from ${url}- status code ${response.status}`,
+		);
+	}
+
+	const rawPosts: wordpressApiPost[] = await response.json();
+
+	const posts = rawPosts.map((post) => ({
+		url: post.link,
+		title: he.decode(post.title.rendered),
+		description: he.decode(
+			post.yoast_head_json?.og_description ?? post.excerpt.rendered,
+		),
+		date: new Date(post.date_gmt),
+	}));
+
+	return {
+		posts,
+		totalPages: parseInt(response.headers.get("x-wp-totalpages")!),
+	};
+}
 
 export function wordpressProvider(opts: wordpressProviderOpts): FeedProvider {
-	async function fetchPage(page: number) {
-		const url = new URL(
-			`/wp-json/wp/v2/posts?page=${page}&per_page=100${opts.categoryFilter ? `&categories=${opts.categoryFilter}` : ""}`,
-			opts.baseUrl,
-		).toString();
-
-		const response = await fetch(url);
-
-		if (response.status !== 200) {
-			throw new Error(
-				`failed to fetch posts from ${url}- status code ${response.status}`,
-			);
-		}
-
-		const rawPosts: wordpressApiPost[] = await response.json();
-
-		const posts = rawPosts.map((post) => ({
-			url: post.link,
-			title: he.decode(post.title.rendered),
-			description: he.decode(
-				post.yoast_head_json?.og_description ?? post.excerpt.rendered,
-			),
-			date: new Date(post.date_gmt),
-		}));
-
-		return {
-			posts,
-			totalPages: parseInt(response.headers.get("x-wp-totalpages")!),
-		};
-	}
+	const fetchPageThrottled = asyncThrottle(
+		async (page: number) => await fetchPage(page, opts),
+		{
+			wait: 200,
+		},
+	);
 
 	return {
 		type: "wordpress",
@@ -40,7 +48,8 @@ export function wordpressProvider(opts: wordpressProviderOpts): FeedProvider {
 		fetch: async () => {
 			const posts: PostData[] = [];
 
-			const firstPage = await fetchPage(1);
+			const firstPage = (await fetchPageThrottled(1))!;
+
 			posts.push(...firstPage.posts);
 
 			if (firstPage.totalPages === 1) {
@@ -57,7 +66,7 @@ export function wordpressProvider(opts: wordpressProviderOpts): FeedProvider {
 
 			await Promise.all(
 				arr.map(async (pageNumber) => {
-					const page = await fetchPage(pageNumber);
+					const page = (await fetchPageThrottled(pageNumber))!;
 					posts.push(...page.posts);
 				}),
 			);
