2e7c3a9eba47ade761f7df5962aeb4b09966a43d
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-07-10 15:45:43 -0400

    install redis

---
+++ src/util/redis.ts
@@ -0,0 +1,11 @@
+import { Redis } from "@upstash/redis";
+
+import {
+	UPSTASH_REDIS_REST_URL,
+	UPSTASH_REDIS_REST_TOKEN,
+} from "astro:env/server";
+
+export const redis = new Redis({
+	url: UPSTASH_REDIS_REST_URL,
+	token: UPSTASH_REDIS_REST_TOKEN,
+});
