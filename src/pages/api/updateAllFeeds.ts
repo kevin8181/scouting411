1072af3996869750e07772c44a8871894d641e0e
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2026-03-11 17:13:51 -0400

    secure the updateAllFeeds endpoint

--- src/pages/api/updateAllFeeds.ts
+++ src/pages/api/updateAllFeeds.ts
@@ -1,12 +1,17 @@
 import type { APIRoute } from "astro";
 import { FeedManager } from "@/features/feeds/feedManager";
+import { CRON_SECRET } from "astro:env/server";
 
 export const prerender = false;
 
-//todo secure this endpoint with cron secret https://vercel.com/docs/cron-jobs/manage-cron-jobs?framework=other#securing-cron-jobs
+export const GET: APIRoute = async (context) => {
+	const authHeader = context.request.headers.get("authorization");
 
-export const GET: APIRoute = async () => {
+	if (authHeader !== `Bearer ${CRON_SECRET}`) {
+		return new Response("401 Unauthorized", { status: 401 });
+	}
+
 	await FeedManager.updateAllFeeds();
 
-	return new Response();
+	return new Response("200 OK", { status: 200 });
 };
