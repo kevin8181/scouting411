800cf988d7a35efd3b34eaa9570781686d9cdac1
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2026-07-23 15:35:35 -0400

    sidebar fixes

--- src/components/layout/sidebar/appShell.tsx
+++ src/components/layout/sidebar/appShell.tsx
@@ -1,18 +1,21 @@
 import type { ReactNode } from "react";
 import { AppSidebar } from "@/components/layout/sidebar/appSidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export function AppShell({ url, children }: { url: URL; children: ReactNode }) {
 	return (
-		<SidebarProvider className="flex h-dvh w-full flex-col">
-			<header className="bg-gray-1 border-gray-3 flex h-13 items-center justify-end border-b px-5 py-3">
-				<SidebarTrigger className="md:hidden" />
-			</header>
-
-			<div className="flex h-[calc(100vh-52px)]">
-				<AppSidebar url={url} />
-				<main className="h-full w-full overflow-auto">{children}</main>
-			</div>
+		<SidebarProvider>
+			<AppSidebar url={url} />
+			<SidebarInset>
+				<header className="sticky top-0 bg-gray-1 border-gray-3 flex h-13 items-center justify-end border-b px-5 py-3">
+					<SidebarTrigger className="md:hidden" />
+				</header>
+				{children}
+			</SidebarInset>
 		</SidebarProvider>
 	);
 }
