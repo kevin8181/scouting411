bb9b0b4f7a31378bb86e8dd88ad7d0d5748eed96
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2026-07-27 01:38:13 -0400

    alphabetize list of feeds

--- src/lib/news/feeds/feedManager.ts
+++ src/lib/news/feeds/feedManager.ts
@@ -1,8 +1,8 @@
 import { hydrateFeed } from "@/lib/news/feeds/feed";
 import { feedConfigs } from "@/lib/news/config";
 
-/** the list of all feeds, hydrated */
-export const feeds = feedConfigs.map(hydrateFeed);
+/** the list of all feeds, hydrated and alphabetized */
+export const feeds = feedConfigs.map(hydrateFeed).sort((a, b) => a.name.localeCompare(b.name));
 
 /** gets a feed by its slug */
 export function getFeedBySlug(slug: string) {
