c1d03d90168a75bca9e88548e6a933f2e88a61ef
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2026-07-28 18:17:37 -0400

    format

--- src/components/react/filterSidebarItem.tsx
+++ src/components/react/filterSidebarItem.tsx
@@ -7,7 +7,9 @@ export function FilterSidebarItem({
 }) {
 	return (
 		<div className="flex flex-col gap-3 p-3">
-			<span className="text-muted-foreground text-xs font-bold uppercase">{label}</span>
+			<span className="text-muted-foreground text-xs font-bold uppercase">
+				{label}
+			</span>
 			{children}
 		</div>
 	);
